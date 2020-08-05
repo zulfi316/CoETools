8d62bb0532e2029c4d4f50821a6f897248949e65
Zulfiqar Tapia <zulfi316@gmail.com>
2020-08-05 19:53:22 +0000

    Removing globally namespaced functions

--- Code/PPTGenerator/code.js
+++ Code/PPTGenerator/code.js
@@ -1,309 +1,321 @@
-function processDataSet() {
+function PPTGenerator() {
 
-	// Get everything the user entered -
-	var sDataSet = document.querySelector("#dataSet").value;
+	return {
+		processDataSet: function () {
 
-	// No input? Stop
-	if (sDataSet.trim() === "")
-		throw new Error("- No input -");
+			// Get everything the user entered -
+			var sDataSet = document.querySelector("#dataSet").value;
 
-	// Split by new line -
-	var aDataSet = sDataSet.split(/\r\n|\n|\r/);
+			// No input? Stop
+			if (sDataSet.trim() === "")
+				throw new Error("- No input -");
 
-	// First step will be to get all the different parameters in the passed in dataset
+			// Split by new line -
+			var aDataSet = sDataSet.split(/\r\n|\n|\r/);
 
-	// Stage 1 - Limit to user logins and sessions
+			// First step will be to get all the different parameters in the passed in dataset
 
-	var oResultSet = {};
-	var iDateIndex = 0,
-		iTimeIndex = 0,
-		iAppServerIndex = 0;
+			// Stage 1 - Limit to user logins and sessions
 
-	// TODO: Add this back into result set
-	var sDate = aDataSet[0].trim().split(' ')[0];
+			var oResultSet = {};
+			var iDateIndex = 0,
+				iTimeIndex = 0,
+				iAppServerIndex = 0;
 
-	// Get the raw measures -
-	var aMeasureList = aDataSet[4].split('|').map(measure => measure.trim());
+			// TODO: Add this back into result set
+			var sDate = aDataSet[0].trim().split(' ')[0];
 
-	//Convert to key index pair -
-	for (let i = 0; i < aMeasureList.length; i++) {
+			// Get the raw measures -
+			let iMeasuresIndex = 0;
+			for (iMeasuresIndex = 0; iMeasuresIndex < aDataSet.length; iMeasuresIndex++) {
+				if (aDataSet[iMeasuresIndex].indexOf("AS Instance") !== -1)
+					break;
+			}
+			var aMeasureList = aDataSet[iMeasuresIndex].split('|').map(measure => measure.trim());
 
-		let sCurrentMeasure = aMeasureList[i];
+			//Convert to key index pair -
+			for (let i = 0; i < aMeasureList.length; i++) {
 
-		if (sCurrentMeasure) {
-			// Remove trailing spaces from the measure -
-			sCurrentMeasure.trim();
+				let sCurrentMeasure = aMeasureList[i];
 
-			if (sCurrentMeasure !== "") {
+				if (sCurrentMeasure) {
+					// Remove trailing spaces from the measure -
+					sCurrentMeasure.trim();
 
-				// Is it one of the special measures?
-				if (sCurrentMeasure.toLowerCase() == "date" && iDateIndex === 0)
-					iDateIndex = i;
+					if (sCurrentMeasure !== "") {
 
-				else if (sCurrentMeasure.toLowerCase() == "time" && iTimeIndex === 0)
-					iTimeIndex = i;
+						// Is it one of the special measures?
+						if (sCurrentMeasure.toLowerCase() == "date" && iDateIndex === 0)
+							iDateIndex = i;
 
-				else if (sCurrentMeasure.toLowerCase() == "as instance" && iAppServerIndex === 0)
-					iAppServerIndex = i;
+						else if (sCurrentMeasure.toLowerCase() == "time" && iTimeIndex === 0)
+							iTimeIndex = i;
 
-				else if (oResultSet[sCurrentMeasure]) {
-					oResultSet[sCurrentMeasure + "-1"] = {
-						"index": i
-					};
-				} else {
-					oResultSet[sCurrentMeasure] = {
-						"index": i
-					};
+						else if (sCurrentMeasure.toLowerCase() == "as instance" && iAppServerIndex === 0)
+							iAppServerIndex = i;
+
+						else if (oResultSet[sCurrentMeasure]) {
+							oResultSet[sCurrentMeasure + "-1"] = {
+								"index": i
+							};
+						} else {
+							oResultSet[sCurrentMeasure] = {
+								"index": i
+							};
+						}
+					}
 				}
 			}
-		}
-	}
 
-	var aKnownMeasures = Object.keys(oResultSet);
+			var aKnownMeasures = Object.keys(oResultSet);
 
-	var aCurrentLine;
-	// Start from line 6 // TODO: Make more fool-proof
-	for (let i = 6; i < aDataSet.length; i++) {
+			var aCurrentLine;
 
-		// Break the line into several pieces -
-		aCurrentLine = aDataSet[i].split("|");
+			// Start from line 6 // TODO: Make more fool-proof
+			for (let i = iMeasuresIndex + 2; i < aDataSet.length; i++) {
 
-		// Either a blank line or a line with -----------
-		if (aCurrentLine.length < 2)
-			continue;
+				// Break the line into several pieces -
+				aCurrentLine = aDataSet[i].split("|");
 
-		// Now start a loop for all known measures
-		for (let j = 0; j < aKnownMeasures.length; j++) {
-			// Break it down -
+				// Either a blank line or a line with -----------
+				if (aCurrentLine.length < 2)
+					continue;
 
-			// Get the JSON Object associated specifically with this measures -
-			let oCurrentMeasure = oResultSet[aKnownMeasures[j]];
+				// Now start a loop for all known measures
+				for (let j = 0; j < aKnownMeasures.length; j++) {
+					// Break it down -
 
-			// oResultSet[aKnownMeasures[j]][aCurrentLine[iAppServerIndex]]
-			// In that JSON Object is there already info about this app server?
-			if (!oCurrentMeasure[aCurrentLine[iAppServerIndex]])
-				oCurrentMeasure[aCurrentLine[iAppServerIndex]] = {
-					"TimeStamps": [],
-					"Values": []
-				};
+					// Get the JSON Object associated specifically with this measures -
+					let oCurrentMeasure = oResultSet[aKnownMeasures[j]];
 
-			var oCurrentMeasureAppServer = oCurrentMeasure[aCurrentLine[iAppServerIndex]];
-			oCurrentMeasureAppServer.TimeStamps.push(aCurrentLine[iTimeIndex]);
-			try {
-				sCurrentLineValue = aCurrentLine[oCurrentMeasure.index];
+					// oResultSet[aKnownMeasures[j]][aCurrentLine[iAppServerIndex]]
+					// In that JSON Object is there already info about this app server?
+					if (!oCurrentMeasure[aCurrentLine[iAppServerIndex]])
+						oCurrentMeasure[aCurrentLine[iAppServerIndex]] = {
+							"TimeStamps": [],
+							"Values": []
+						};
 
-				sCurrentLineValue = parseFloat(sCurrentLineValue.replace(",", ""));
+					var oCurrentMeasureAppServer = oCurrentMeasure[aCurrentLine[iAppServerIndex]];
+					oCurrentMeasureAppServer.TimeStamps.push(aCurrentLine[iTimeIndex]);
+					try {
+						sCurrentLineValue = aCurrentLine[oCurrentMeasure.index];
 
-				if (isNaN(sCurrentLineValue)) {
-					throw new Error("Encountred a value that is not a number")
-					debugger;
+						sCurrentLineValue = parseFloat(sCurrentLineValue.replace(",", ""));
+
+						if (isNaN(sCurrentLineValue)) {
+							debugger;
+							throw new Error("Encountred a value that is not a number")
+						}
+						oCurrentMeasureAppServer.Values.push(sCurrentLineValue);
+					} catch (e) {
+						alert(e);
+						debugger;
+					}
 				}
-				oCurrentMeasureAppServer.Values.push(sCurrentLineValue);
-			} catch (e) {
-				alert(e);
-				debugger;
 			}
-		}
-	}
-	oResultSet.Date = sDate;
+			oResultSet.Date = sDate;
 
-	// Data will look like this -
-	// {
-	// 	"UserLogins": {
-	//		"index": 7
-	// 		"AppServer 1":{
-	// 		"time-stamps": [],
-	// 		"values": []
-	// 		},
-	//		"AppServer 2":{
-	// 		"time-stamps": [],
-	// 		"values": []
-	// 		}
-	// 	}
-	// }
+			// Data will look like this -
+			// {
+			// 	"UserLogins": {
+			//		"index": 7
+			// 		"AppServer 1":{
+			// 		"time-stamps": [],
+			// 		"values": []
+			// 		},
+			//		"AppServer 2":{
+			// 		"time-stamps": [],
+			// 		"values": []
+			// 		}
+			// 	}
+			// }
 
-	return oResultSet;
-}
+			return oResultSet;
+		},
 
-function beginProcessing() {
+		beginProcessing: function () {
 
-	var aChartData = processDataSet();
+			var aChartData = this.processDataSet();
 
-	// Data will look like this -
-	// {
-	// 	"UserLogins": {
-	//		"index": 7
-	// 		"AppServer 1":{
-	// 		"time-stamps": [],
-	// 		"values": []
-	// 		},
-	//		"AppServer 2":{
-	// 		"time-stamps": [],
-	// 		"values": []
-	// 		}
-	// 	}
-	// }
+			// Data will look like this -
+			// {
+			// 	"UserLogins": {
+			//		"index": 7
+			// 		"AppServer 1":{
+			// 		"time-stamps": [],
+			// 		"values": []
+			// 		},
+			//		"AppServer 2":{
+			// 		"time-stamps": [],
+			// 		"values": []
+			// 		}
+			// 	}
+			// }
 
-	var pptx = new PptxGenJS();
+			var pptx = new PptxGenJS();
 
-	var aAllMeasures = Object.keys(aChartData);
+			var aAllMeasures = Object.keys(aChartData);
 
-	// Do a tweak here to show user info first -
-	//Logins and Sessions
-	if (aAllMeasures.indexOf("Sessions") !== -1) {
-		aAllMeasures = aAllMeasures.filter(items => items !== "Sessions");
-		aAllMeasures = ["Sessions", ...aAllMeasures];
-	}
+			// Do a tweak here to show user info first -
+			//Logins and Sessions
+			if (aAllMeasures.indexOf("Sessions") !== -1) {
+				aAllMeasures = aAllMeasures.filter(items => items !== "Sessions");
+				aAllMeasures = ["Sessions", ...aAllMeasures];
+			}
 
-	if (aAllMeasures.indexOf("Logins") !== -1) {
-		aAllMeasures = aAllMeasures.filter(items => items !== "Logins");
-		aAllMeasures = ["Logins", ...aAllMeasures];
-	}
+			if (aAllMeasures.indexOf("Logins") !== -1) {
+				aAllMeasures = aAllMeasures.filter(items => items !== "Logins");
+				aAllMeasures = ["Logins", ...aAllMeasures];
+			}
 
-	for (let i = 0; i < aAllMeasures.length; i++) {
+			for (let i = 0; i < aAllMeasures.length; i++) {
 
-		let oSpecificInfo = aChartData[aAllMeasures[i]];
+				let oSpecificInfo = aChartData[aAllMeasures[i]];
 
-		// Maybe we have something that isn't a measure? // TODO: Strengthen check
-		if (!oSpecificInfo.index) continue;
+				// Maybe we have something that isn't a measure? // TODO: Strengthen check
+				if (!oSpecificInfo.index) continue;
 
-		let slide = pptx.addSlide();
-		slide.addText(getFriendlyText(aAllMeasures[i]), {
-			x: 0.5,
-			y: 0.7,
-			w: 8,
-			fontSize: 24
-		});
-
-		slide.addText("--- Add Analysis Here ---", {
-			x: 0.5,
-			y: 4.25,
-			w: 8,
-			h: 0.5,
-			isTextBox: true,
-			line: {
-				pt: '2',
-				color: 'A9A9A9'
-			},
-			fontSize: 20
-		});
-
-		var aAppServers = Object.keys(oSpecificInfo);
-
-		var aAppServerChartInfo = [];
-		for (let j = 0; j < aAppServers.length; j++) {
-			let sAppServerName = aAppServers[j];
-
-			let oSpecificAppServerInfo = oSpecificInfo[sAppServerName];
-
-			if (oSpecificAppServerInfo.TimeStamps && oSpecificAppServerInfo.Values) {
-				aAppServerChartInfo.push({
-					name: sAppServerName,
-					labels: oSpecificAppServerInfo.TimeStamps,
-					values: oSpecificAppServerInfo.Values
+				let slide = pptx.addSlide();
+				slide.addText(this.getFriendlyText(aAllMeasures[i]), {
+					x: 0.5,
+					y: 0.7,
+					w: 8,
+					fontSize: 24
 				});
 
+				slide.addText("--- Add Analysis Here ---", {
+					x: 0.5,
+					y: 4.25,
+					w: 8,
+					h: 0.5,
+					isTextBox: true,
+					line: {
+						pt: '2',
+						color: 'A9A9A9'
+					},
+					fontSize: 20
+				});
+
+				var aAppServers = Object.keys(oSpecificInfo);
+
+				var aAppServerChartInfo = [];
+				for (let j = 0; j < aAppServers.length; j++) {
+					let sAppServerName = aAppServers[j];
+
+					let oSpecificAppServerInfo = oSpecificInfo[sAppServerName];
+
+					//			Not adding index
+					if (oSpecificAppServerInfo.TimeStamps && oSpecificAppServerInfo.Values) {
+						aAppServerChartInfo.push({
+							name: sAppServerName,
+							labels: oSpecificAppServerInfo.TimeStamps,
+							values: oSpecificAppServerInfo.Values
+						});
+
+					}
+				}
+
+				slide.addChart(pptx.ChartType.line, aAppServerChartInfo, {
+					x: 0.5,
+					y: 1,
+					w: 8,
+					h: 3,
+					valAxisMinVal: 0,
+					lineSize: 1,
+					lineDataSymbol: "none",
+					lineSmooth: true,
+					showLegend: true,
+					legendPos: 'r',
+					catGridLine: {
+						color: 'D8D8D8',
+						style: 'none',
+						size: 1
+					},
+					valGridLine: {
+						color: 'D8D8D8',
+						style: 'dash',
+						size: 1
+					}
+				});
+			}
+
+			var oGenerateButton = document.querySelector("#reportGenerateButton");
+
+			oGenerateButton.disabled = true;
+			oGenerateButton.textContent = "Generating...";
+
+			pptx.writeFile('SDF SMON Report')
+				.then(function (fileName) {
+					oGenerateButton.disabled = false;
+					oGenerateButton.textContent = "Generate Report"
+
+					document.querySelector("#successMessageHolder").innerText = 'Saved! File Name: ' + fileName;
+
+					setTimeout(function () {
+						document.querySelector("#successMessageHolder").innerText = ""
+					}, 5000);
+				});
+		},
+
+		getFriendlyText: function (sTitle) {
+
+			switch (sTitle) {
+			case "Act. WPs":
+				return "Number of Active Work Processes";
+			case "Dia.WPs":
+				return "Number of Active Dialog Work Processes";
+			case "RFC WPs":
+				return "Number of available WPs for RFCs";
+			case "CPU Usr":
+				return "CPU Utilization (User)";
+			case "CPU Sys":
+				return "CPU Utilization (System)";
+			case "CPU Idle":
+				return "CPU Utilization (Idle)";
+			case "CPU.":
+				return "CPUs Consumed";
+			case "Ava.":
+				return "Available CPUs";
+			case "Rea.":
+				return "Ready Time in %";
+			case "Ste.":
+				return "Steal Time in Seconds";
+			case "Paging in":
+				return "Paging in (% of RAM per hour)";
+			case "Paging out":
+				return "Paging out  (% of RAM per hour)";
+			case "Free Mem.":
+				return "Free Memory in % of RAM";
+			case "FreeMem":
+				return "Free Memory (MB)";
+			case "Free(+FS)":
+				return "Free Memory MB (incl. Filesystem Cache)";
+			case "EM alloc.":
+				return "Allocated Extended Memory in MB";
+			case "EM attach.":
+				return "Attached Extended Memory in MB";
+			case "Heap Memor":
+				return "Heap Memory in MB";
+			case "Pri.":
+				return "Priv Modes";
+			case "Dia.":
+				return "Dialog Queue Length";
+			case "Ave.":
+				return "Average Load last 20s";
+			case "Ave.-1":
+				return "Average Load last 60s";
+			case "Upd.":
+				return "Update Queue Length";
+			case "Enq.":
+				return "Enqueue Queue Length";
+			case "Logins":
+				return "Number of logins";
+			case "Sessions":
+				return "Number of sessions";
+			default:
+				return sTitle;
 			}
 		}
-
-		slide.addChart(pptx.ChartType.line, aAppServerChartInfo, {
-			x: 0.5,
-			y: 1,
-			w: 8,
-			h: 3,
-			valAxisMinVal: 0,
-			lineSize: 1,
-			lineDataSymbol: "none",
-			lineSmooth: true,
-			showLegend: true,
-			legendPos: 'r',
-			catGridLine: {
-				color: 'D8D8D8',
-				style: 'none',
-				size: 1
-			},
-			valGridLine: {
-				color: 'D8D8D8',
-				style: 'dash',
-				size: 1
-			}
-		});
-	}
-
-	var oGenerateButton = document.querySelector("#reportGenerateButton");
-
-	oGenerateButton.disabled = true;
-	oGenerateButton.textContent = "Generating...";
-
-	pptx.writeFile('SDF SMON Report')
-		.then(function (fileName) {
-			oGenerateButton.disabled = false;
-			oGenerateButton.textContent = "Generate Report"
-
-			document.querySelector("#successMessageHolder").innerText = 'Saved! File Name: ' + fileName;
-
-			setTimeout(function () {
-				document.querySelector("#successMessageHolder").innerText = ""
-			}, 5000);
-		});
-}
-
-function getFriendlyText(sTitle) {
-
-	switch (sTitle) {
-	case "Act. WPs":
-		return "Number of Active Work Processes";
-	case "Dia.WPs":
-		return "Number of Active Dialog Work Processes";
-	case "RFC WPs":
-		return "Number of available WPs for RFCs";
-	case "CPU Usr":
-		return "CPU Utilization (User)";
-	case "CPU Sys":
-		return "CPU Utilization (System)";
-	case "CPU Idle":
-		return "CPU Utilization (Idle)";
-	case "CPU.":
-		return "CPUs Consumed";
-	case "Ava.":
-		return "Available CPUs";
-	case "Rea.":
-		return "Ready Time in %";
-	case "Ste.":
-		return "Steal Time in Seconds";
-	case "Paging in":
-		return "Paging in (% of RAM per hour)";
-	case "Paging out":
-		return "Paging out  (% of RAM per hour)";
-	case "Free Mem.":
-		return "Free Memory in % of RAM";
-	case "FreeMem":
-		return "Free Memory (MB)";
-	case "Free(+FS)":
-		return "Free Memory MB (incl. Filesystem Cache)";
-	case "EM alloc.":
-		return "Allocated Extended Memory in MB";
-	case "EM attach.":
-		return "Attached Extended Memory in MB";
-	case "Heap Memor":
-		return "Heap Memory in MB";
-	case "Pri.":
-		return "Priv Modes";
-	case "Dia.":
-		return "Dialog Queue Length";
-	case "Ave.":
-		return "Average Load last 20s";
-	case "Ave.-1":
-		return "Average Load last 60s";
-	case "Upd.":
-		return "Update Queue Length";
-	case "Enq.":
-		return "Enqueue Queue Length";
-	case "Logins":
-		return "Number of logins";
-	case "Sessions":
-		return "Number of sessions";
-	default:
-		return sTitle;
 	}
 }
